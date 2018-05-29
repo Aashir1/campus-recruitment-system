378f71e2464aa5d3bd9b1755163976b8f6242130
Aashir <kaashir9@gmail.com>
2018-05-29 15:25:40 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Routes from './Routes'
+import store from './store';
+import DrawerUndockedExample from './Component/Navbar/Navbar';
+ReactDOM.render(
+    <Provider store={store}>
+        <MuiThemeProvider>
+            <div>
+                <DrawerUndockedExample />
+                <Routes />
+            </div>
+        </MuiThemeProvider>
+    </Provider>
+    , document.getElementById('root'));
+registerServiceWorker();
